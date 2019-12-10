ba11c6f8623193420a8437b5ba3e51852ae946cb
sutgon <sutgon@gmail.com>
2019-12-10 19:13:45 +0200

    Add Black Lion Industry Contract

--- src/static/gemstoreItems.js
+++ src/static/gemstoreItems.js
@@ -1044,5 +1044,6 @@ export default {
   92264: {gems: 350, flags: []}, // Shiverpeaks Hunter Glider (Inside a container)
   92398: {gems: 350, flags: []}, // Shiverpeaks Hunter Backpack (Inside a container)
   92526: {gems: 500, flags: []}, // Emblazoned Cape
-  92472: {gems: 600, flags: []} // Dreamwalker Dagger Skin
+  92472: {gems: 600, flags: []}, // Dreamwalker Dagger Skin
+  92427: {gems: 1000, flags: []} // Black Lion Industry Contract
 }
